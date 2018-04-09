b5e75b8336b3a87b768802a19704a65defec7780
Alan0555 <alan0555@qq.com>
2018-04-09 12:50:16 +0800

    提交
    Signed-off-by: Alan0555 <alan0555@qq.com>

--- ourOwn/js/index.js
+++ ourOwn/js/index.js
@@ -1,6 +1,11 @@
 var vm = new Vue({
   el: '#app',
   data: function () {
+    return {
+      advertisementLabel:'最新消息：',
+      advertisementContent1:'通知：第032期开奖时间 2018年03月29日 星期四.',
+      advertisementContent2:' 提示:首次访问本站请添加到书签,以防下次找不到本站',
+    }
 
   },
 })
